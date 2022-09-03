a73d97d1f1e9d3afa32d02f0170c9a3ad80e4f10
hari-pd <haribhandari2611@gmail.com>
2022-09-04 01:20:21 +0300

    starting again

---
+++ Name_address.py
@@ -0,0 +1,8 @@
+### ask more user input and store the values in variable
+
+g_name=input("Given name: ")
+f_name=input("Family name: ")
+address=input("Street address: ")
+city=input("City and postal code: ")
+##print using f-string
+print(f"{g_name} {f_name}\n{address}\n{city}")
---
+++ name_twice.py
@@ -0,0 +1,4 @@
+## Ask for the users name and print it twice
+
+name=input("What is your name?")
+print(name+"\n"+name)
